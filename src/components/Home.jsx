e6ddea079ef4c5e0ea9bca4248b391352fdf5409
chinmayi <chinmayishaan2316@gmail.com>
2024-07-10 01:44:54 +0530

    Portfolio

---
+++ src/components/Home.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+import { HiArrowNarrowRight } from "react-icons/hi";
+
+const Home = () => {
+  return (
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
+      {/* Container */}
+      <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <p className="text-pink-600">Hi 👋, my name is</p>
+        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+          CHINMAYI D S
+        </h1>
+        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+          I'm a Full Stack Developer.
+        </h2>
+        <p className="text-[#8892b0] py-4 max-w-[700px]">
+          I am a passionate coder, driven by a relentless curiosity and a love
+          for technology. Self-taught and constantly learning, I thrive on
+          tackling real-life problems through innovative solutions. My journey
+          in coding is fueled by a deep desire to create meaningful impact and
+          improve everyday experiences with the power of code.
+        </p>
+        <div>
+          <button href="" className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
+            View Work
+            <span className="group-hover:rotate-90 duration-300">
+              <HiArrowNarrowRight className="ml-3 " />
+            </span>
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/data/data.js
@@ -0,0 +1,19 @@
+import NITK from "../assets/projects/NITK-SOC.png";
+import Weather from "../assets/projects/weatherapp.png";
+
+export const data = [
+  {
+    id: 1,
+    name: "NITK- Summer Of Code",
+    image: NITK,
+    github: "https://github.com/urgetolearn/NITK-Summer-Of-Code",
+    live: "https://nitk-summer-of-code.vercel.app/",
+  },
+  {
+    id: 2,
+    name: "Weather App",
+    image: Weather,
+    github: "https://github.com/urgetolearn/Final-weather-app",
+    live: "https://urgetolearn.github.io/Final-weather-app/",
+  },
+];
